969ca196d895acbcf1181b7d6491d70867678108
Nataliya Khromykh <nataliyakhromykh@Nataliyas-MBP.attlocal.net>
2023-08-31 12:49:14 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from 'react';
+import video from './video.mp4';
+
 
 function App() {
+  const [advice,setAdvice] = useState(false);
+  const getAdvice = async () => {
+    const response = await fetch('http://www.boredapi.com/api/activity/');
+    const data = await response.json();
+    setAdvice(data.activity)
+  }
+  useEffect(() =>{
+    getAdvice()
+  }, [])
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bored? We have some ideas for you!</h1>
+
+      <div>
+        <button onClick={getAdvice}>💡</button>
+        {advice && <MyComponent/>}
+      </div>
+
+
+      <div className='container'>
+        <video autoPlay muted loop>
+          <source src={video} type="video/mp4"/>
+        </video>
+      </div>
+      
     </div>
   );
+}
+
+function MyComponent(){
+  return <div className='container'>
+  <p>{advice}</p>
+</div>
 }
 
 export default App;
